d1e6105f5550ac350b311b32b01117e9527d9b82
Septian Panjaya <102370228+panjayaseptian16@users.noreply.github.com>
2023-11-01 11:34:52 +0000

    Changes to be committed: 	modified:   pages/05_Feedback.py

--- pages/05_Feedback.py
+++ pages/05_Feedback.py
@@ -31,13 +31,13 @@ contact_form = """
      <input type="hidden" name="_captcha" value="false">
      <input type="hidden" name="_template" value="table">
      <p>Rate Our Project:</p>
-    <fieldset class="rating" required>
+     <fieldset class="rating" required>
         <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
         <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
         <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
         <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
         <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Terrible - 1 star"></label>
-    </fieldset>
+     </fieldset>
      <button type="submit">Send</button>
 </form> 
 """
