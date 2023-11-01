3da5dea56817e6245ed464f9761a49a744864ea7
Septian Panjaya <102370228+panjayaseptian16@users.noreply.github.com>
2023-11-01 02:32:58 +0000

    Changes to be committed: 	modified:   overview.py

--- overview.py
+++ overview.py
@@ -2,8 +2,6 @@ import streamlit as st
 from streamlit.components.v1 import html
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
-from streamlit_folium import folium_static
-import folium
 
 
 # Adding a sidebar with the required elements
@@ -26,31 +24,3 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-
-def read_file(file_path):
-    with open(file_path, "r") as file:
-        content = file.read()
-    return content
-
-tes_html = read_file("map1.html")
-
-col1, col2 = st.columns([4,1],gap='medium')
-with col1:
-        html(tes_html, height=425)
-with col2:
-        m = folium.Map(location=[-6.2088, 106.8456], zoom_start=11, control_scale=True)
-        folium.TileLayer('openstreetmap').add_to(m)
-        waqi_tile_layer = folium.TileLayer(
-                tiles='https://tiles.aqicn.org/tiles/usepa-aqi/{z}/{x}/{y}.png?token=3f5ba5c57e7bbe5460dda6475203d0e5e91fb9b8',
-                attr='Air Quality',
-                name='Air Quality',
-                overlay=True,
-                control=True
-            )
-        waqi_tile_layer.add_to(m)
-        folium.LayerControl().add_to(m)
-        # Display the map in Streamlit
-        folium_static(m)
-        if col2.button('Reset'):
-            m.location = [-6.2088, 106.8456]
-            m.zoom_start = 11
