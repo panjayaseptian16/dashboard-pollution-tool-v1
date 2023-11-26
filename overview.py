f862b19940e43bd220d1acfc0f1d8e7cb77b5912
Septian Panjaya <102370228+panjayaseptian16@users.noreply.github.com>
2023-11-27 02:07:01 +0800

    Update overview.py

--- overview.py
+++ overview.py
@@ -16,13 +16,6 @@ st.set_page_config(
 # app to add indendation in the sidebar
 show_pages_from_config()
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown("""
           <!DOCTYPE html>
